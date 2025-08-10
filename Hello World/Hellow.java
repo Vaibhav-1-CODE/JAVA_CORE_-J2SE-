4d269ec7c3a69e2533bd608f340e298480738048
SALUNKHE VAIBHAV <vaibhavsalunkhe019@gmail.com>
2025-08-10 10:36:49 +0000

    Hello World
    HelloWorld -Exp

---
+++ Hello World/Hellow.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+ public  class Hellow 
+ {
+    
+     public static void main(String args[])
+      {
+      
+         System.out.println("Hello World");	
+      }
+     	
+     
+
+ }
+
+ //improt=It Mean You Are Making Availiable Required Libery To Your Application.
+
+//public=If You Want To Make Things (data & Methods) Availiable To Everyone (Externsl World).
+
+//static args[]=It Is An Array Used To Store Command Line Argunment-/It Is A Command Line Argument.
+
+//void=Method Is Not Going To Return Any Things,That's Why it's Return Type Is Void.
+
+//main()=We Are Not Calling main() Method,It's Gets Called When We Run The Java Program From Command Prompt.
+
+//System=system is a class.
+
+//out= out Is Object Of Printstream Class,Decleared In System Class As a 'static'.
+
+//println=print() Is Non-static Method Which Decleared In Printstream Class.
+ 
