132ace588f8844023ef0f9fcd0aca189f5eaeb57
SALUNKHE VAIBHAV <vaibhavsalunkhe019@gmail.com>
2025-08-17 17:38:24 +0000

    WorkingOfArray

---
+++ ARRAY/ArrayIntialization.java
@@ -0,0 +1,21 @@
+/*Array Intialization*/
+
+
+
+
+import java.io.*;
+
+	public class ArrayIntialization
+		{
+			public static void main(String args[])
+            {
+				int a[]= {11,12,13};
+
+				System.out.print("Array :");
+
+						for(int i=0; i<a.length;i++)
+							{
+                                System.out.print(a[i] + " ");
+							} 
+		    }
+		}    
---
+++ ARRAY/ReferenceLength.java
@@ -0,0 +1,25 @@
+/*Checking ,Array Reference and Length*/
+
+import java.io.*;
+
+
+	public class ReferenceLength
+		{
+
+			public static void main(String args[])
+				{
+					int a[]=new int[3];
+
+					System.out.println("Reference value :" +a);// for reference value =[I@473b46c3
+					System.out.println("Numbers Of Array You Will Intiulization :" +a.length);// Length Of Array =3
+                    
+                    System.out.println("Printing The Index Of - a");
+
+                    System.out.println(a[0]);//0
+                    System.out.println(a[1]);//0
+                    System.out.println(a[2]);//0
+
+
+
+				}
+		}
