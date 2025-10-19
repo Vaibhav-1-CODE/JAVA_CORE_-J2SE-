d26952351a32abc5938d3d05b711acbc372fd0bf
SALUNKHE VAIBHAV <vaibhavsalunkhe019@gmail.com>
2025-10-19 11:40:14 +0000

    Hello World
    TYPES  [ Hello World]

---
+++ Hello World/Hellow1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+
+
+class H1
+{
+	static
+	{
+		System.out.print("HELLO WORLD!!!");
+	}
+}
+
+public class Hellow1
+{
+	public static void main(String[] args )
+	{
+
+		  H1 ob = new H1();
+
+
+	}
+}
